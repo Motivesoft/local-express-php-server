55fc0730fd576aa136987210b3f3187fbe5f774a
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2025-03-17 10:37:15 +0100

    Use the current logged in session if there is one

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hello World</title>
+    <link rel="stylesheet" href="style.css">
     <script src="/script.js"></script>
 </head>
 <body>
@@ -14,10 +15,22 @@
     </h1>
     <p>
         <?php
+        session_start();
+                
         $currentTime = date("Y-m-d H:i:s");
         echo "The current date and time is: " . $currentTime;
+        echo "<br>";
+        echo "The current user is '" . $_SESSION['username'] . "'";
+        echo "<br>";
+        echo "<button type='button' class='button' onclick='login()'>Log in</button>"
         ?>
-        <script>console.log("Hello");</script>
+        <script>
+            console.log("Index page");
+
+            function login() {
+                window.location.href="/auth/login.php";
+            }
+        </script>
     </p>
 </body>
 </html>
